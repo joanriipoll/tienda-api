ce61be80d29c5059ae39972b60097bfc9ec6d873
Joan Ripoll Blanes <jripoll@heligrafics.net>
2026-07-06 13:01:18 +0000

    refactor: remove unused PRODUCT_ID_COUNTER from test fixture

--- tests/store-api/conftest.py
+++ tests/store-api/conftest.py
@@ -2,12 +2,10 @@ from typing import Generator
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
-from app.database import INVENTORY, PRODUCT_ID_COUNTER
+from app.database import INVENTORY
 
 @pytest.fixture(scope="function")
 def client() -> Generator[TestClient, None, None]:
-    global PRODUCT_ID_COUNTER
     with TestClient(app) as test_client:
         yield test_client
     INVENTORY.clear()
-    PRODUCT_ID_COUNTER = 1
